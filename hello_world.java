790b9cdeb604ba967e538c28de72102cf4db3abb
Sven <svenwu996@gmail.com>
2021-05-11 15:40:22 -0700

    Created first Java file.
    First Java file using Visual Studio Code.

---
+++ hello_world.java
@@ -0,0 +1,31 @@
+/*
+# Java is a high level, modern programming language. 
+# It is portable and can run on a different number of platforms.
+# "Write Once, Run Anywhere."
+
+
+# In Java, every line of code that can actually run needs to be inside a class.
+# In our example, we named the class 'myClass'. 
+# In Java, each application has an entry point, or a starting point, which is a method called main. 
+*/
+class myClass{
+    public static void main(String[] args){
+        System.out.println("Greetings Master Sven.");
+    }
+}
+/* 
+# The method's parameters are declared inside the parentheses that follow the name of the method.
+# For main, it's an array of strings called args.
+
+# Remember: do not use semicolons after method and class declarations...
+...that follow with the body defined using the curly braces.
+
+# public = anyone can access it.
+# static = method can be run without creating an instance of the class.
+# void = method does not return any value.
+# main = the name of the method.
+
+# For example, the following code declares a method called test,...
+# ...which does not return anything and has no parameters.
+# void test();
+*/
