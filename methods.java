95bb100ddcad773200608d657ecf87822e3a9f61
Sven <svenwu996@gmail.com>
2021-05-11 19:28:56 -0700

    Basic methods made with Andrew.

---
+++ methods.java
@@ -0,0 +1,24 @@
+/** This is a documentation comment */
+class methods {
+    public String name = "Andrew";
+
+    public void jarvis(String name) {
+        System.out.println("Greetings Master " + name);
+    }
+
+    public static void friday(String dork) {
+        System.out.println("Go to hell " + dork);
+    }
+
+    public static void main(String[] args) {
+        methods m = new methods();
+        m.jarvis("Sven");
+
+        m.jarvis("Mom");
+
+        methods.friday("Andrew");
+    }
+
+}
+
+
