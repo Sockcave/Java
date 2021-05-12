4acb41f0210e111d06cc3eb9fc654042f94b410c
Sven <svenwu996@gmail.com>
2021-05-12 11:10:25 -0700

    Updated scanner and created math_ops file.

---
+++ math_ops.java
@@ -0,0 +1,56 @@
+// Math operands include (+, -, *, /, %)
+
+public class math_ops {
+
+    // custom add method
+    public static void add(int c, int d) {
+        int result = c + d;
+        System.out.println(result);
+    }
+
+    // custom multiplication method
+    public static void multiply(int sum1, int sum2) {
+        int sum3 = sum1 * sum2;
+        System.out.println(sum3);
+    }
+
+    // custom division method
+    public static void divide(int sum1, int sum2) {
+        int sum3 = sum1 / sum2;
+        System.out.println(sum3);
+    }
+
+    // dividing doubles or floats
+    public static void divide_doubles(double sum1, double sum2) {
+        double sum3 = sum1 / sum2;
+        System.out.println(sum3);
+    }
+
+    // custom modulo method
+    public static void modulo(int val1, int val2) {
+        int val3 = val1 % val2;
+        System.out.println(val3);
+    }
+
+    public static void main(String[] args) {
+        int x = 6 * 3;
+        System.out.println(x);
+
+        int a = 2; int b = 4;
+        int result = a + b;
+        System.out.println(result);
+
+        math_ops.add(9, 6);
+
+        math_ops.multiply(1000*2, 1000*2*10);
+
+        math_ops.divide(1000/5, (100/5)/2);
+
+        math_ops.divide(15, 4);
+
+        math_ops.divide_doubles(15.0, 4.0);
+
+        math_ops.modulo(23, 6);
+    }
+    
+}
--- scanner.java
+++ scanner.java
@@ -1,8 +1,24 @@
+// Scanner object lets us get user input. 
+
+/*
+Here are some methods that are available through the Scanner class:
+Read a byte - nextByte()
+Read a short - nextShort()
+Read an int - nextInt()
+Read a long - nextLong()
+Read a float - nextFloat()
+Read a double - nextDouble()
+Read a boolean - nextBoolean()
+Read a complete line - nextLine()
+Read a word - next()
+*/
+
+// Import the scanner class to use.
 import java.util.Scanner;
 
 public class scanner {
     public static void main(String[] args) {
-         // create an instance of the class by using the following syntax
+         // create an instance of the class by using the following syntax to use it.
         Scanner myVar = new Scanner(System.in);
         System.out.println("Please enter some text boi.");
         System.out.println(myVar.nextLine());
