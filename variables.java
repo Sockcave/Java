f5ec248b857dd9fc3d6c57b5ee95d410794232fe
Sven <svenwu996@gmail.com>
2021-05-12 10:42:33 -0700

    Created variables file.

---
+++ variables.java
@@ -0,0 +1,18 @@
+// Variables store data for processing. It is givena  name(identifier)
+// Variables have data types (int, String, double, char, boolean)
+
+//char data type can hold one character
+// boolean data type can hold True or False
+
+class variables{
+    public static void main(String[] args) {
+        String name = "David";
+        int age = 42;
+        double score = 15.9;
+        char group = 'Z';
+        boolean online = true;
+
+        System.out.println("Name is " + name + ". Age is " + age + ". Score is " + score + ". Group is " 
+        + group + ". Online status is " + online);
+    }
+}
