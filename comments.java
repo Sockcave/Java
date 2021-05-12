583a8bad77d842d228a54e4639887655408ef6e6
Sven <svenwu996@gmail.com>
2021-05-11 19:00:47 -0700

    Created comments file

---
+++ comments.java
@@ -0,0 +1,11 @@
+/* The purpose of including comments in your Java code is to explain what the code is doing.
+*/
+
+class comments {
+    public static void main(String[] args){
+         // This is a single-line comment.
+        int x = 5; // A single line comment after some code.
+        System.out.println(x);
+    }
+}
+
