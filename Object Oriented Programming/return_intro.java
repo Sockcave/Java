8374446e7386e7cccd4372fa939eb205665f9aac
Sven <svenwu996@gmail.com>
2021-05-13 19:42:16 -0700

    Updated return keyword file.

--- Object Oriented Programming/return_intro.java
+++ Object Oriented Programming/return_intro.java
@@ -10,8 +10,40 @@ public class return_intro {
         return val1 + val2;
     }
 
+    //returns an int value of 5
+    static int returnFive() {
+        return 5;
+    }
+
+    // has a parameter
+    static void sayHelloTo(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    // simply prints "Hello World."
+    static void sayHello() {
+        System.out.println("Hello World.");
+    }
+
+    // Return the max value
+    static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    // This definition indicates that the main method takes an array of Strings as its parameters, ...
+    //..and does not return a value.
     public static void main(String[] args) {
         int x = sum(2, 5);
         System.out.println(x);
+
+        returnFive();
+        sayHelloTo("Zuko");
+        sayHello();
+
+        max(9, 6);
     }
 }
